a7435400518ba1e4821e57764de08d41f4fab0e3
Dusan <dusan.daniska@wai.sk>
2024-12-02 22:06:56 +0100

    download document

--- clients/php/src/Traits/Documents.php
+++ clients/php/src/Traits/Documents.php
@@ -8,6 +8,7 @@ trait Documents
   /**
    * Shortcut to create a document.
    *
+   * @param  mixed $folderUid UID of the folder where is document located.
    * @param  mixed $document Content of the new document.
    * @return string DocumentUid in case of 200 success. Otherwise exception is thrown.
    */
@@ -20,6 +21,7 @@ trait Documents
   /**
    * Shortcut to update a document
    *
+   * @param  mixed $folderUid UID of the folder where is document located.
    * @param  mixed $documentUid UID of the document to update.
    * @param  mixed $newContent New documents's content.
    * @return string DocumentUid in case of 200 success. Otherwise exception is thrown.
@@ -27,6 +29,32 @@ trait Documents
   public function updateDocument(string $folderUid, string $documentUid, string $newContent): string
   {
     $res = $this->sendRequest("PUT", "/database/{$this->database}/folder/{$folderUid}/document/{$documentUid}", ['newContent' => $newContent]);
+    return (string) $res->getBody();
+  }
+
+  /**
+   * Get document's metadata
+   *
+   * @param  mixed $folderUid UID of the folder where is document located.
+   * @param  mixed $documentUid UID of the document to update.
+   * @return string DocumentUid in case of 200 success. Otherwise exception is thrown.
+   */
+  public function getDocument(string $folderUid, string $documentUid): array
+  {
+    $res = $this->sendRequest("GET", "/database/{$this->database}/folder/{$folderUid}/document/{$documentUid}");
+    return json_decode((string) $res->getBody(), true);
+  }
+
+  /**
+   * Download document
+   *
+   * @param  mixed $folderUid UID of the folder where is document located.
+   * @param  mixed $documentUid UID of the document to update.
+   * @return string Content of the document in case of 200 success. Otherwise exception is thrown.
+   */
+  public function downloadDocument(string $folderUid, string $documentUid): string
+  {
+    $res = $this->sendRequest("GET", "/database/{$this->database}/folder/{$folderUid}/document/{$documentUid}/download");
     return (string) $res->getBody();
   }
 
